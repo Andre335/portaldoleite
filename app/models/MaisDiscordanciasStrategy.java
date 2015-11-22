e59de7b33f442b0ab1bc84168f4daf0249e51bda
Andre335 <andre335@gmail.com>
2015-11-22 20:07:18 -0200

    Tarefa 3 completa

--- app/models/MaisDiscordanciasStrategy.java
+++ app/models/MaisDiscordanciasStrategy.java
@@ -2,6 +2,7 @@ package models;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class MaisDiscordanciasStrategy implements FilterStrategy {
@@ -26,4 +27,19 @@ public class MaisDiscordanciasStrategy implements FilterStrategy {
 		}
 	}
 
+}
+
+class MaisDiscordanciasComparator implements Comparator<Dica> {
+
+	@Override
+	public int compare(Dica o1, Dica o2) {
+		if (o1.getDiscordancias()>o2.getDiscordancias()) {
+			return -1;
+		} else if (o1.getDiscordancias()<o2.getDiscordancias()) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+	
 }
--- app/models/MaisVotosPositivosStrategy.java
+++ app/models/MaisVotosPositivosStrategy.java
@@ -28,3 +28,18 @@ public class MaisVotosPositivosStrategy implements FilterStrategy {
 	}
 
 }
+
+class MaisVotosComparator implements Comparator<Dica> {
+
+	@Override
+	public int compare(Dica o1, Dica o2) {
+		if (o1.getConcordancias()>o2.getConcordancias()) {
+			return -1;
+		} else if (o1.getConcordancias()<o2.getConcordancias()) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+	
+}
