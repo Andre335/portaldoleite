46444bb4fb2d3f35074306a0fda2960d5a7c4131
Nicolas Gabriel <nicolasgabrielt@gmail.com>
2015-11-26 10:25:40 -0200

    Criação do gerenciador de Dicas

---
+++ app/models/GerenciadorDeDica.java
@@ -0,0 +1,40 @@
+package models;
+
+import java.util.Map;
+
+import models.dao.GenericDAO;
+import models.dao.GenericDAOImpl;
+
+public class GerenciadorDeDica {
+	
+	private Dica dica;
+	
+	public GerenciadorDeDica(){
+		this.dica = new DicaAssunto("sem texto");
+	}
+	
+	
+	public GerenciadorDeDica(Dica dica){
+		this.dica = dica;
+	}
+
+
+	public Dica getDica() {
+		return dica;
+	}
+
+
+	public void setDica(Dica dica) {
+		this.dica = dica;
+	}
+	
+	public void salvarDica(Tema tema, String userName , GenericDAOImpl dao){		
+		tema.addDica(dica);
+		dica.setTema(tema);
+		dica.setUser(userName);
+		dao.persist(dica);
+		
+	}
+	
+
+}
